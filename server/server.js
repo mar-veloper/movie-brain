819c50d6546c893b27424dc584490fb61dcb0cc3
Jonmar <jonmar21yan@gmail.com>
2020-11-04 16:05:37 +0100

    init proj

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+//Define port
+var port = 3000;
+//Define request response in root URL (/)
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+//Launch listening server on port 3000
+app.listen(port, function () {
+  console.log("app listening on port ${port}!");
+});
